4636fb7a935b3f0c71f6096416bc3e31b930a515
itsaditya01 <adityachuahan9974@gmail.com>
2023-03-09 11:22:02 +0530

    created functionality of adding image into canvas

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="canvas-ground">
     <div class="side-bar">
-      <button class='btn btn-dark m-1' @click="addText">Add Text</button>
-      <button class='btn btn-dark m-1' @click="addRectangle">Add Box</button>
+      <button class="btn btn-dark m-1" @click="addText">Add Text</button>
+      <button class="btn btn-dark m-1" @click="addRectangle">Add Box</button>
+      <button class="btn btn-dark m-1" @click="addImage">Add Image/Logo</button>
+      <button class="btn btn-dark m-1" @click="logJson">Save</button>
     </div>
-    <div class="editor m-1 p-1 ">
+    <div class="editor m-1 p-1">
       <canvas id="canvas"></canvas>
     </div>
     <div v-if="textbox">
-      <TextController :canvas="canvas" :textbox="textbox" :key="textboxId.id"></TextController>
+      <TextController
+        :canvas="canvas"
+        :textbox="textbox"
+        :key="textboxId.id"
+      ></TextController>
     </div>
     <div v-if="rectangle">
-      <RectController :canvas="canvas" :rectangle="rectangle" :key="rectangleId.id"></RectController>
+      <RectController
+        :canvas="canvas"
+        :rectangle="rectangle"
+        :key="rectangleId.id"
+      ></RectController>
     </div>
   </div>
 </template>
 
 <script>
-import { fabric } from 'fabric';
-import TextController from './components/TextControls.vue';
-import RectController from './components/RectControls.vue';
+import { fabric } from "fabric";
+import TextController from "./components/TextControls.vue";
+import RectController from "./components/RectControls.vue";
+import LogoImg from "./assets/logo.png";
 
 export default {
   components: { TextController, RectController },
@@ -28,9 +39,9 @@ export default {
       canvas: null,
       textObjects: [],
       rectObjects: [],
-      textboxId: '',
-      rectangleId: '',
-    }
+      textboxId: "",
+      rectangleId: "",
+    };
   },
   computed: {
     textbox() {
@@ -38,29 +49,32 @@ export default {
     },
     rectangle() {
       return this.rectangleId;
-    }
+    },
   },
   mounted() {
-    const canvas = new fabric.Canvas('canvas', { backgroundColor: 'white', height: 800, width: 600 });
+    const canvas = new fabric.Canvas("canvas", {
+      backgroundColor: "white",
+      height: 800,
+      width: 600,
+    });
     this.canvas = canvas;
 
-
-    this.canvas.on('mouse:down', (e) => {
+    this.canvas.on("mouse:down", (e) => {
       if (!e.target) {
         this.textboxId = null;
         this.rectangleId = null;
         return;
       }
-      if (e.target.type === 'rect') {
+      if (e.target.type === "rect") {
         this.textboxId = null;
         this.rectangleId = e.target;
-      } else if (e.target.type === 'textbox') {
+      } else if (e.target.type === "textbox") {
         this.rectangleId = null;
         this.textboxId = e.target;
       }
     });
     this.canvas.preserveObjectStacking = true;
-    this.canvas.on('selection:cleared', () => {
+    this.canvas.on("selection:cleared", () => {
       this.textboxId = null;
       this.rectangleId = null;
     });
@@ -68,31 +82,46 @@ export default {
   methods: {
     addRectangle() {
       const rectangle = new fabric.Rect({
-        fill: '#06538e',
+        fill: "#06538e",
         width: 125,
         height: 125,
         zIndex: 1,
-        id: 'rectObj' + this.rectObjects.length
-      })
+        id: "rectObj" + this.rectObjects.length,
+      });
       this.rectObjects.push(rectangle);
       this.canvas.add(rectangle);
       console.log(rectangle);
     },
     addText() {
-      const text = new fabric.Textbox('Sample Text', {
-        fontFamily: 'Arial',
+      const text = new fabric.Textbox("Sample Text", {
+        fontFamily: "Arial",
         fontSize: 24,
-        fill: 'black',
-        backgroundColor: 'transparent',
+        fill: "black",
+        backgroundColor: "transparent",
         selectable: true,
         zIndex: 2,
-        id: 'textObj' + this.textObjects.length
-      })
-      this.textObjects.push(text)
-      this.canvas.add(text)
-    }
-  }
-}
+        id: "textObj" + this.textObjects.length,
+      });
+      this.textObjects.push(text);
+      this.canvas.add(text);
+    },
+    addImage: function () {
+      const image = new window.Image();
+      image.src = LogoImg;
+      image.onload = () => {
+        let imgInstance = new fabric.Image(image, {
+          left: 0,
+          top: 0,
+        });
+        imgInstance.scaleToWidth(this.canvas.getWidth() / 4);
+        this.canvas.add(imgInstance);
+      };
+    },
+    logJson: function () {
+      console.log(this.canvas.toJSON());
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -101,7 +130,6 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
-
 }
 
 .side-bar {
